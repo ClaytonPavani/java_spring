7c946efc085a87bf98c00485964978431afd6d8a
ClaytonPavani <claytonpavani@gmail.com>
2024-04-17 14:34:42 -0300

    conclusão do aplicativo

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -9,10 +9,7 @@ import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -86,15 +83,21 @@ public class Principal {
                 .collect(Collectors.toList());
 
 
-        System.out.println("\nTop 5 episodios:");
-        dadosEpisodios.stream()
-                //aqui estamos ordenando - e como parâmetro estamos comparando a avaliação de todas entradas de DadosEpisodio
-                // O "reversed" significa que ele vai ordenar da forma contrária, do Melhor ao Pior:
-                // Filter - Criamos um filtro que o avaliação é diferente, ignorecase "N/A"
-                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
-                .limit(5)
-                .forEach(System.out::println);
+//        System.out.println("\nTop 10 episodios:");
+//        dadosEpisodios.stream()
+//                //aqui estamos ordenando - e como parâmetro estamos comparando a avaliação de todas entradas de DadosEpisodio
+//                // O "reversed" significa que ele vai ordenar da forma contrária, do Melhor ao Pior:
+//                // Filter - Criamos um filtro que o avaliação é diferente, ignorecase "N/A"
+//                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                // Criando um Peek para observar as operações encadeadas do JAVA
+//                .peek(e -> System.out.println("Primeiro filtro (N/A) " + e))
+//                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+//                .peek(e -> System.out.println("Ordenação " + e))
+//                .limit(10)
+//                .peek(e -> System.out.println("Limite " + e))
+//                .map(e -> e.titulo().toUpperCase())
+//                .peek(e -> System.out.println("Mapeamento " + e))
+//                .forEach(System.out::println);
 
         // Criando uma nova lista, transformando dados da lista de temporadas, em uma lista de episódios
         List<Episodio> episodios = temporadas.stream()
@@ -104,30 +107,63 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
-        // Criando uma pergunta - Sobre um filtro de data:
-        System.out.println("A partir de que ano você deseja ver os episódios? ");
-        // Vamos criar uma variável que irá receber um Scanner de leitura
-        var ano = leitura.nextInt();
-        leitura.nextLine();
+//        System.out.println("Digite um trecho do título do episódio: ");
+//        // Criamos uma Variável que o usuário irá digitar e vamos atribuir essa variável ao trecho titulo:
+//        var trechoTitulo = leitura.nextLine();
+//        Optional<Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+//                .findFirst();
+//        if(episodioBuscado.isPresent()){
+//            System.out.println("Episódio encontrado");
+//            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+//        } else {
+//            System.out.println("Episódio não encontrado!");
+//        }
 
-        // Criando uma variável do Tipo Local Date, para armazenarmos a data de busca:
-        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+//        // Criando uma pergunta - Sobre um filtro de data:
+//        System.out.println("A partir de que ano você deseja ver os episódios? ");
+//        // Vamos criar uma variável que irá receber um Scanner de leitura
+//        var ano = leitura.nextInt();
+//        leitura.nextLine();
+//
+//        // Criando uma variável do Tipo Local Date, para armazenarmos a data de busca:
+//        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+//
+//
+//        // Criando um formatador prévio - para converter o formato de data para o padrão Brasileiro:
+//        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//
+//
+//        // Utilizando Streams combinada com método .isAfter() criando um filtro de data
+//        episodios.stream()
+//                .filter(e -> e.getDataDeLancamento() != null && e.getDataDeLancamento().isAfter(dataBusca))
+//                // forEach -> Imprimindo tempora, episódios e data de lançamento - Concatenando informações
+//                .forEach(e -> System.out.println(
+//                        " Temporada: " + e.getTemporada() +
+//                                " Episódio: " + e.getTitulo() +
+//                                " Data de Lançamento: " + e.getDataDeLancamento().format(formatador)
+//                ));
+
+        // Criando um Mapa - Que irá percorrer os episódios de cada temproada e a avaliação de cada episódio da temporada
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                // Criando um filtro que pega a avaliação e compara se é maior que 0.0
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avaliacoesPorTemporada);
 
 
-        // Criando um formatador prévio - para converter o formato de data para o padrão Brasileiro:
-        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-
-        // Utilizando Streams combinada com método .isAfter() criando um filtro de data
-        episodios.stream()
-                .filter(e -> e.getDataDeLancamento() != null && e.getDataDeLancamento().isAfter(dataBusca))
-                // forEach -> Imprimindo tempora, episódios e data de lançamento - Concatenando informações
-                .forEach(e -> System.out.println(
-                        " Temporada: " + e.getTemporada() +
-                                " Episódio: " + e.getTitulo() +
-                                " Data de Lançamento: " + e.getDataDeLancamento().format(formatador)
-                ));
-
+        // Utilizando Classe Double Summary Para exibir estatística da nossa API
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        // Usando o método associado a variável para imprimir a média:
+        System.out.println("Media: " + est.getAverage());
+        // Usando o método associado a variável para imprimir o minimo e o máximo, quantidade total
+        System.out.println("Melhor Episódio: " + est.getMax());
+        System.out.println("Pior Episodio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
 
 
     }
